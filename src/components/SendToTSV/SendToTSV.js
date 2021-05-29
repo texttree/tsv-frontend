ba189d6c4cb775060efd2e9b0f7c3e0f21c0c46b
Valyukhov <alexzed@bk.ru>
2021-05-29 19:14:27 +0300

    Check reference

--- src/components/SendToTSV/SendToTSV.js
+++ src/components/SendToTSV/SendToTSV.js
@@ -4,43 +4,43 @@ import {Books,Types} from './config'
 
 function SendToTSV({
     reference,
-    bookId,
-    type,
-    resourse,
-    fields:{
-      quote,
-      comment
-    }
+     bookId,
+    // type,
+    // resource,
+    // fields:{
+    //   quote,
+    //   comment
+    // }
    
   
 }) {
   
-  const checkReference = (\d+\:\d+([abc]?)(\-\d+)?[abc]?)
+  const checkReference = (reference.match(/\d+\:\d+([abc]?)(\-\d+)?[abc]?/))?true:false;
   const checkBookId = (Books.includes(bookId))?true:false;
   const checkType = (Types.includes(type))?true:false;
-  // if reference === 
+  console.log(checkBookId);
 
   
-   const handleSend =()=> {
+  //  const handleSend =()=> {
   
-    fetch(reference.serverLink, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body:'type=' + 
-      encodeURIComponent(reference.resource) +
-      '&bookId=' + 
-      encodeURIComponent(reference.bookId) +
-      '&ref=' + 
-      encodeURIComponent(reference.chapter+':'+reference.verse) +
-      '&selected=' +
-      encodeURIComponent(reference.textSelected) +
-      '&comment=' +
-      encodeURIComponent(reference.comment) 
-    })}
+  //   fetch(reference.serverLink, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form-urlencoded',
+  //     },
+  //     body:'type=' + 
+  //     encodeURIComponent(reference.resource) +
+  //     '&bookId=' + 
+  //     encodeURIComponent(reference.bookId) +
+  //     '&ref=' + 
+  //     encodeURIComponent(reference.chapter+':'+reference.verse) +
+  //     '&selected=' +
+  //     encodeURIComponent(reference.textSelected) +
+  //     '&comment=' +
+  //     encodeURIComponent(reference.comment) 
+  //   })}
       
-    handleSend()
+  //   handleSend()
   
   
   return (null );
--- src/components/SendToTSV/SendToTSV.md
+++ src/components/SendToTSV/SendToTSV.md
@@ -4,20 +4,12 @@ ObjToTsv
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { SendToTSV } from "@texttree/tsv-frontend";
-const reference = {
-  resource: "rob",
-  bookId: "gen",
-  chapter: "1",
-  verse: "2",
-  serverLink: "https://lit-falls-48214.herokuapp.com/send-to-file",
-  textSelected: "Word",
-  comment: "test",
-};
 
 <Button
   variant="contained"
   onClick={SendToTSV({
-    reference: { ...reference },
+    reference: "1:1",
+    bookId: "gen",
   })}
 >
   Send
