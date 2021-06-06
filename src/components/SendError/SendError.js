aa907eeb5d62e2d47349ba8a9b3955a1198a7e64
Valyukhov <alexzed@bk.ru>
2021-06-06 06:27:58 +0300

    fix fields

--- src/components/SendError/SendError.js
+++ src/components/SendError/SendError.js
@@ -1,21 +1,21 @@
-import { SendToTSV } from '../../components';
+import { SendToTSV } from "../../components";
 
 function SendError({ reference, bookId, resource, serverLink, fields }) {
-  if (fields?.quote && fields?.comment) {
+  if (fields?.Quote && fields?.Note) {
     return SendToTSV({
       reference,
       bookId,
       resource,
       serverLink,
-      fields: { quote: fields.quote, comment: fields.comment },
-      type: 'err',
+      fields: { Quote: fields.Quote, Note: fields.Note },
+      type: "err",
     });
   } else {
     return {
       success: false,
       code: 100,
-      message: 'Fields not set', // текстовый вариант сообщения
-      errors: [{ field: 'fields', error: 'Need quote and comment' }],
+      message: "Fields not set", // текстовый вариант сообщения
+      errors: [{ field: "fields", error: "Need quote and comment" }],
     };
   }
 }
