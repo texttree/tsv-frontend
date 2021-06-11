2c2c8db18707d3f7f91e6d0a0b5e7704b0a1c84d
Vitek Foxprogs <foxprogs@gmail.com>
2021-06-11 21:43:02 +0400

    add promise to SendToTSV

--- src/components/SendToTSV/SendToTSV.js
+++ src/components/SendToTSV/SendToTSV.js
@@ -8,60 +8,63 @@ import {
 } from '../../common/validation';
 
 function SendToTSV({ reference, bookId, type, resource, serverLink, fields }) {
-  const response = {
-    success: true, // true or false
-    code: 200, // нужно в документации все варианты кодов ошибок указать
-    message: 'Sending was successful', // текстовый вариант сообщения
-    errors: [],
-    /** массив из ошибок для валидации полей.
-     * что-то типа [
-     *  {field: 'reference', error: 'invalid format'},
-     *  {field: 'fields', error: 'empty'}
-     * ]  */
-  };
+  let promise = new Promise((resolve, reject) => {
+    const response = {
+      success: true, // true or false
+      code: 200, // нужно в документации все варианты кодов ошибок указать
+      message: 'Sending was successful', // текстовый вариант сообщения
+      errors: [],
+      /** массив из ошибок для валидации полей.
+       * что-то типа [
+       *  {field: 'reference', error: 'invalid format'},
+       *  {field: 'fields', error: 'empty'}
+       * ]  */
+    };
 
-  const checkBlock = {
-    checkReference: [referenceValidation, reference, 'reference'],
-    checkBookId: [bookIdValidation, bookId, 'bookId'],
-    checkType: [typeValidation, type, 'type'],
-    checkResourse: [resourceValidation, resource, 'resource'],
-    checkFields: [fieldsValidation, fields, 'fields'],
-  };
+    const checkBlock = {
+      checkReference: [referenceValidation, reference, 'reference'],
+      checkBookId: [bookIdValidation, bookId, 'bookId'],
+      checkType: [typeValidation, type, 'type'],
+      checkResourse: [resourceValidation, resource, 'resource'],
+      checkFields: [fieldsValidation, fields, 'fields'],
+    };
 
-  Object.entries(checkBlock).map(([check, [validation, value, name]]) => {
-    check = validation(value);
-    if (check.error) {
-      response.success = false;
-      response.code = 100;
-      response.message = 'Validation error';
-      response.errors.push({
-        field: name,
-        message: check.message,
-      });
+    Object.entries(checkBlock).map(([check, [validation, value, name]]) => {
+      check = validation(value);
+      if (check.error) {
+        response.success = false;
+        response.code = 100;
+        response.message = 'Validation error';
+        response.errors.push({
+          field: name,
+          message: check.message,
+        });
+      }
+    });
+
+    if (response.success) {
+      axios
+        .post(serverLink, {
+          type,
+          resource,
+          bookId,
+          reference,
+          fields,
+        })
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((error) => {
+          response.success = false;
+          response.code = 101;
+          response.message = 'Server error';
+          response.errors = error;
+          reject(response);
+        });
+    } else {
+      reject(response);
     }
   });
-
-  if (response.success) {
-    axios
-      .post(serverLink, {
-        type,
-        resource,
-        bookId,
-        reference,
-        fields,
-      })
-      .then((res) => {
-        return res;
-      })
-      .catch((error) => {
-        response.success = false;
-        response.code = 101;
-        response.message = 'Server error';
-        response.errors = error;
-        return response;
-      });
-  } else {
-    return response;
-  }
+  return promise;
 }
 export default SendToTSV;
--- src/components/SendToTSV/SendToTSV.md
+++ src/components/SendToTSV/SendToTSV.md
@@ -8,16 +8,24 @@ import { SendToTSV } from '@texttree/tsv-frontend';
 const [openDialog, setOpenDialog] = useState(false);
 const [answerSend, setAnswerSend] = useState({});
 const handleClick = () => {
-  const answer = SendToTSV({
+  SendToTSV({
     reference: '2:1',
     bookId: 'gen',
     resource: 'rlob',
     serverLink: 'https://tsv-backend.herokuapp.com/send',
     type: 'err',
     fields: { Note: 'note', Quote: 'quote' },
-  });
-  setOpenDialog(true);
-  setAnswerSend(answer);
+  })
+    .then((res) => {
+      console.log('res', res);
+      setOpenDialog(true);
+      setAnswerSend(res);
+    })
+    .catch((err) => {
+      console.log('err', err);
+      setOpenDialog(true);
+      setAnswerSend(err);
+    });
 };
 const handleClose = () => {
   setOpenDialog(false);
